ea3a1c8828da4104f1c78369f78ca0230d81eb03
SutiwatNarksri <Sutiwat.narksri@gmail.com>
2024-11-10 15:13:13 +0700

    Update app_predict_penguin_66130701919.py

--- app_predict_penguin_66130701919.py
+++ app_predict_penguin_66130701919.py
@@ -2,8 +2,8 @@ import pickle
 import pandas as pd
 import streamlit as st
 # ฟังก์ชันทำนายประเภทของเพนกวิน
-def predict_penguin(species_features):
-    prediction = model.predict([species_features])
+def predict_penguin(prediction):
+    #prediction = model.predict([species_features])
     return species_dict[prediction[0]]  # แปลงผลลัพธ์เป็นชื่อสายพันธุ์
 # โหลดโมเดลและตัวแปลงที่บันทึกไว้
 with open('model_penguin_66130701919.pkl', 'rb') as file:
